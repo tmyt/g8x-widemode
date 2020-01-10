d8a7b0d76346f05278880c20845365227ed0e4ce
yutaka <shigure@refy.net>
2020-01-11 04:51:08 +0900

    add debug buttons

--- app/src/debug/java/net/refy/android/g8x/widemode/activities/DebugActivity.kt
+++ app/src/debug/java/net/refy/android/g8x/widemode/activities/DebugActivity.kt
@@ -2,52 +2,51 @@ package net.refy.android.g8x.widemode.activities
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.os.IBinder
-import kotlinx.android.synthetic.main.debug_activity.*
+import android.widget.Button
 import net.refy.android.g8x.widemode.R
 import net.refy.android.g8x.widemode.utils.ActivityUtils
+import net.refy.android.reflect.Reflect
 
 class DebugActivity : Activity() {
+    class LGPowerManagerHelper(context: Context) : Reflect() {
+        class LGContext(context: Context) : Reflect() {
+            override val type: Class<*> = Class.forName("com.lge.systemservice.core.LGContext")
+            override val value = ctor(Context::class.java)(context)
+            val getLGSystemService by virtual<Any>(String::class.java)
+        }
+
+        private val lgContext = LGContext(context)
+        override val value = lgContext.getLGSystemService("lgpowermanagerhelper")
+        override val type = value.javaClass
+        val checkDualDisplayPowerOn by virtual<Unit>(Boolean::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.debug_activity)
 
-        val cServiceManager = Class.forName("android.os.ServiceManager")
-        val mGetService = cServiceManager.getMethod("getService", String::class.java)
-        val className = "android.hardware.display.IDisplayManagerEx\$Stub"
-        val cStub = Class.forName(className)
-        val mAsInterface = cStub.getDeclaredMethod("asInterface", IBinder::class.java)
-        val iface = mAsInterface.invoke(null, mGetService.invoke(null, "display"))
-
-        //private val displayManager = mAsInterface(null, (context.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager).
-        //private val mGetCoverDisplayState = displayManager.javaClass.getDeclaredMethod("getCoverDisplayState")
-
-//        val am = getSystemService(Context.WINDOW_SERVICE)
-//        val clazz = am.javaClass
-//        clazz.methods.forEach {
-//            Log.i("XX", it.name)
-//        }
-//
-//        val c = Class.forName("com.lge.config.Features")
-//        c.fields.forEach {
-//            Log.i("XX", it.name)
-//        }
-
         val activityUtils = ActivityUtils(this)
+        val lgPowerManagerHelper = LGPowerManagerHelper(this)
 
-        val LGContext = Class.forName("com.lge.systemservice.core.LGContext")
-        val lgcontext = LGContext.getConstructor(Context::class.java).newInstance(applicationContext)
-        val lgPowerManagerHelper =
-            LGContext.getMethod("getLGSystemService", String::class.java).invoke(lgcontext, "lgpowermanagerhelper")
-        val checkDualDisplayPowerOn =
-            lgPowerManagerHelper.javaClass.getMethod("checkDualDisplayPowerOn", Boolean::class.java);
-        screen_on.setOnClickListener { checkDualDisplayPowerOn.invoke(lgPowerManagerHelper, true) }
-        screen_off.setOnClickListener { checkDualDisplayPowerOn.invoke(lgPowerManagerHelper, false) }
-        widemode_on.setOnClickListener { activityUtils.setWideMode(true) }
-        widemode_off.setOnClickListener { activityUtils.setWideMode(false) }
-        move_to_cover.setOnClickListener { activityUtils.moveToDisplayAsDisplayId(0, 0) }
-        move_from_cover.setOnClickListener { activityUtils.moveToDisplayAsDisplayId(1, 0) }
-        move_switch.setOnClickListener { activityUtils.moveToDisplayEx(2) }
+        findViewById<Button>(R.id.screen_on).setOnClickListener { lgPowerManagerHelper.checkDualDisplayPowerOn(true) }
+        findViewById<Button>(R.id.screen_off).setOnClickListener { lgPowerManagerHelper.checkDualDisplayPowerOn(false) }
+        findViewById<Button>(R.id.widemode_on).setOnClickListener { activityUtils.setWideScreenMode(true) }
+        findViewById<Button>(R.id.widemode_off).setOnClickListener { activityUtils.setWideScreenMode(false) }
+        findViewById<Button>(R.id.move_to_cover).setOnClickListener { activityUtils.moveToDisplayAsDisplayId(0, 0) }
+        findViewById<Button>(R.id.move_from_cover).setOnClickListener { activityUtils.moveToDisplayAsDisplayId(1, 0) }
+        findViewById<Button>(R.id.move_switch).setOnClickListener { activityUtils.moveToDisplayEx(2) }
+        findViewById<Button>(R.id.launch_on_cover).setOnClickListener {
+            activityUtils.startActivityInCoverDisplay(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://google.com/")
+                ).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                })
+        }
     }
 }
+
